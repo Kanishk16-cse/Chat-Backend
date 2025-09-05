575882aa22c3908d680354b60c0e654b96f2640f
Kanishk Garg <gargkanishk437@gmail.com>
2025-09-05 23:30:26 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,8 @@ import jwt from "jsonwebtoken";
 // Middleware to protect routes
 export const protectRoute = async (req, res, next)=>{
     try {
-        const token = req.headers.token;
+        const token = req.headers.authorization?.split(" ")[1]; // Get token from "Authorization: Bearer <token>"
+
 
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
